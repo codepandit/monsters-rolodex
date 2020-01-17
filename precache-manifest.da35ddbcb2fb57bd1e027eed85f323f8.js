7765d1025c62983ae6de6750a64cbf6ede2c5953
Nikhil Mehral <nikhil.mehral@hotmail.com>
2020-01-18 00:43:30 +1000

    Updates

---
+++ precache-manifest.da35ddbcb2fb57bd1e027eed85f323f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7806532b73ac866a862e8e832f831df3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f16d092f5c9d4e4b0510",
+    "url": "/monsters-rolodex/static/css/main.78b08834.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f16d092f5c9d4e4b0510",
+    "url": "/monsters-rolodex/static/js/main.87d709ff.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
